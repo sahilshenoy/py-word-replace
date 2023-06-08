b63c1a39a9cbc1380ad0b3ab9f999c3cbeec2e00
Sahil Shenoy <sahilshenoy3@gmail.com>
2023-06-08 15:58:10 +0530

    Update wordreplace.py

--- wordreplace.py
+++ wordreplace.py
@@ -1,4 +1,4 @@
-str = "My name is sahil shenoy, my hobby is to fuck my cuttu who is dhwanu"
+str = input("Enter the String: ")
 word_replace = input("Enter the word to replace: ")
 word_to_replace = input("Enter the word to be replaced with: ")
 print(str.replace(word_replace, word_to_replace))
