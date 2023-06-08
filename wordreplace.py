abe8ecbe139fa456a1995ad47987aaa4e4b4e765
Sahil Shenoy <sahilshenoy3@gmail.com>
2023-06-08 15:29:03 +0530

    Add files via upload

---
+++ wordreplace.py
@@ -0,0 +1,5 @@
+str = "My name is sahil shenoy, my hobby is to fuck my cuttu who is dhwanu"
+word_replace = input("Enter the word to replace: ")
+word_to_replace = input("Enter the word to be replaced with: ")
+print(str.replace(word_replace, word_to_replace))
+    
